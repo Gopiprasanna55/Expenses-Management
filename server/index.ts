024a03e908279628aba486675e9984444c6c90e1
RatnakiranKankipati <kiranratna18@gmail.com>
2025-09-22 15:11:02 +0530

    testing the insted changes when the git commits are add

--- server/index.ts
+++ server/index.ts
@@ -80,6 +80,8 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
+  console.log("Hello World");
+  
   // ALWAYS serve the app on the port specified in the environment variable PORT
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
